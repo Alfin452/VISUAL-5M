1565cdd227bf56a92569635a147c6740b52a7c3d
Nosezzy <151822668+Alfin452@users.noreply.github.com>
2024-12-02 11:49:25 +0800

    update pertemuan 7

--- PERTEMUAN 6/insertdata.py
+++ PERTEMUAN 6/insertdata.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(host = "localhost", user = "root", password = "", database = "db_penjualan")
 
 mycursor= mydb.cursor()
-sql = "INSERT INTO kategori1 (id, name) VALUES (%s, %s)"
+sql = "INSERT INTO kategori (id, name) VALUES (%s, %s)"
 val = ("2", "Drinks")
 mycursor.execute(sql, val)
 
